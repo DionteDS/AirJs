91e26da4a460a838fbeebbe70fc9c5655bb8985a
Dionte <Dionte_sb@yahoo.com>
2019-08-25 21:47:20 -0700

    Added Air Jordans 6 - 13 to the project.

--- AirJs/Model/AirJModel.swift
+++ AirJs/Model/AirJModel.swift
@@ -31,6 +31,30 @@ class AirJordanModel {
         singleShoeObjects = Shoes(shoeName: "Air Jordan V", shoeYear: "Year: 1990", shoeImageName: "aj5", shoeDescription: "Hatfield is believed to have drawn inspiration for the Air Jordan V from World War II fighter planes, which was most notably visible in the shark teeth shapes on the midsole. The Air Jordan V was reintroduced in 2000, including a new colorway featuring Michael Jordan's high school (Laney High) colors.")
         ajShoes.append(singleShoeObjects)
         
+        singleShoeObjects = Shoes(shoeName: "Air Jordan VI", shoeYear: "Year 1992", shoeImageName: "aj6", shoeDescription: "The Air Jordan VI introduced a reinforcement around the toe, It had two holes in the tongue, and a molded heel tab on the back of the sneaker (demanded by Jordan so it wouldn't hit his Achilles tendon).")
+        ajShoes.append(singleShoeObjects)
+        
+        singleShoeObjects = Shoes(shoeName: "Air Jordan VII", shoeYear: "Year 1992", shoeImageName: "aj7", shoeDescription: "When Jordan went to compete at the 1992 Barcelona Olympics to play for the US Men's Basketball Team (also known as the \"Dream Team\"), Nike released a special Olympic color combo of the Air Jordan VII model which had Jordan's Olympic jersey number 9, instead of the usual \"23\" found on other colorways.")
+        ajShoes.append(singleShoeObjects)
+        
+        singleShoeObjects = Shoes(shoeName: "Air Jordan VIII", shoeYear: " Year 1992-1993", shoeImageName: "aj8", shoeDescription: "The eighth model of the Air Jordan was noticeably heavier than its predecessors. The Air Jordan VIII model became known as the \"Punisher\" because of the advanced basketball ankle support and enhanced traction.")
+        ajShoes.append(singleShoeObjects)
+        
+        singleShoeObjects = Shoes(shoeName: "Air Jordan IX", shoeYear: "Year 1993", shoeImageName: "aj9", shoeDescription: "The Air Jordan IX model was the first model released after Michael Jordan's retirement. Jordan never played an NBA season wearing these shoes. This model was inspired by baseball cleats that Jordan wore when playing minor-league baseball.")
+        ajShoes.append(singleShoeObjects)
+        
+        singleShoeObjects = Shoes(shoeName: "Air Jordan X", shoeYear: "Year 1995", shoeImageName: "aj10", shoeDescription: "The shoe also featured all of Michael Jordan's accomplishments up to his first retirement on the outsole.")
+        ajShoes.append(singleShoeObjects)
+        
+        singleShoeObjects = Shoes(shoeName: "Air Jordan XI", shoeYear: "Year 1995-1996", shoeImageName: "aj11", shoeDescription: "The sneakers were only samples in 1995 when Jordan decided to come back to the NBA. Hatfield and Nike discouraged Jordan from playing in them, but once they were produced, he couldn't resist.")
+        ajShoes.append(singleShoeObjects)
+        
+        singleShoeObjects = Shoes(shoeName: "Air Jordan XII", shoeYear: "Year 1996-1997", shoeImageName: "aj12", shoeDescription: "Though not as light weight as previous models, the construction and firmness of the shoe is widely considered to be the most durable and sturdy shoes from the Air Jordan line.")
+        ajShoes.append(singleShoeObjects)
+        
+        singleShoeObjects = Shoes(shoeName: "Air Jordan XIII", shoeYear: "Year 1997", shoeImageName: "aj13", shoeDescription: "In 1997, Air Jordan XIII's were released to the public. This model was known for its cushioning along the feet, designed by Hatfield. The Black Panther was the inspiration for the Air Jordan XIII, the sole resembles the pads on a panther's paw.")
+        ajShoes.append(singleShoeObjects)
+        
     }
     
 }
